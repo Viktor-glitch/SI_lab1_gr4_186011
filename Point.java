88bdcf2278717220c14356bb07426e2b6e427bec
Viktor-glitch <viktor.stamenkovski@students.finki.ukim.mk>
2020-04-11 18:37:01 +0200

    point class finished

--- Point.java
+++ Point.java
@@ -1,15 +1,46 @@
 class Point {
-	String id
+	String id;
 	double x,y;
-	String boja;
-	//TODO add new variable
 
+	//TODO add new variable
+	String boja;
 	//TODO constructor
+	Point(id, x, y, boja){
+		this.id = id;
+		this.x = x;
+		this.y = y;
+		this.boja = boja;
+	}
 
 	//TODO setters and getters
+	void setId(id){
+		this.id = id;
+	}
+	void setId(x){
+		this.x = x;
+	}
+	void setId(y){
+		this.y = y;
+	}
+	void setId(boja){
+		this.boja = boja;
+	}
+	int getX(){
+		return x;
+	}
+	int getY(){
+		return y;
+	}
+	char getId(){
+		return id;
+	}
+	char getBoja(){
+		return boja;
+	}
 
 	public void move (char xDirection, char yDirection) {
 		//TODO
+
 	}
 
 	public void draw () {
